065283af7615d7a63546cdbf6495ce693e9f7939
esposm03 <36164633+esposm03@users.noreply.github.com>
2021-01-12 22:09:42 +0100

    Implement a route to get a post

---
+++ tests/get_post.rs
@@ -0,0 +1,75 @@
+mod common;
+use common::spawn_app;
+
+use my_cms::routes::PostData;
+use sqlx::PgPool;
+use uuid::Uuid;
+use chrono::Utc;
+
+#[actix_rt::test]
+async fn get_post_works() {
+    let app = spawn_app().await;
+    let id = insert_post(&app.db_pool).await;
+
+    let response = reqwest::Client::new()
+        .get(&format!("{}/post?id={}", app.address, id))
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(response.status(), 200);
+
+    let response: PostData = response.json().await.unwrap();
+    assert_eq!(response.title, "Lorem Ipsum");
+    assert_eq!(response.content, "Dolor sit amet");
+}
+
+#[actix_rt::test]
+async fn get_post_wrong_id() {
+    let app = spawn_app().await;
+    insert_post(&app.db_pool).await;
+    let id = Uuid::new_v4();
+
+    let response = reqwest::Client::new()
+        .get(&format!("{}/post?id={}", app.address, id))
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(response.status(), 404);
+}
+
+#[actix_rt::test]
+async fn get_post_no_id() {
+    let app = spawn_app().await;
+    insert_post(&app.db_pool).await;
+
+    let response = reqwest::Client::new()
+        .get(&format!("{}/post", app.address))
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(response.status(), 400);
+}
+
+/// Helper function. This directly inserts a post in the given
+/// database, with title "Lorem Ipsum" and content "Dolor sit
+/// amet", and returns its randomly-generated UUID
+async fn insert_post(pool: &PgPool) -> Uuid {
+    let id = Uuid::new_v4();
+    let title = "Lorem Ipsum";
+    let content = "Dolor sit amet";
+
+    let query = sqlx::query!(
+        r#"
+            INSERT INTO posts (id, title, content, created)
+            VALUES ($1, $2, $3, $4)
+        "#,
+        id,
+        title,
+        content,
+        Utc::now(),
+    );
+
+    query.execute(pool).await.unwrap();
+
+    id
+}
